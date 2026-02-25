3effa1e487dce778809c5bcdb7db1ad577455f6e
sunriseya777 <saayana7777@gmail.com>
2026-02-25 23:17:55 +0700

    strcpy

--- Lab_9/Lab_9.cpp
+++ Lab_9/Lab_9.cpp
@@ -5,7 +5,11 @@ int main()
     setlocale(LC_ALL, "RU");
     char str1[] = "вода";
     char str2[] = "огонь";
-    cout << str1 << " " << str2;
+    char str3[20] = "";
+    cout << str1 << ", " << str2 <<", "<< str3 << "." << endl;
+    // функции char
+    strcpy_s(str3, str1);
+    cout << str1 << ", " << str2 << ", " << str3 << ".";
 
 }
 
