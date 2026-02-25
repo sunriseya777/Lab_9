ea0e72450f03c642f1ab8279c9582e1e2ec18706
sunriseya777 <saayana7777@gmail.com>
2026-02-25 23:24:47 +0700

    strcat

--- Lab_9/Lab_9.cpp
+++ Lab_9/Lab_9.cpp
@@ -3,13 +3,16 @@ using namespace std;
 int main()
 {
     setlocale(LC_ALL, "RU");
-    char str1[] = "вода";
-    char str2[] = "огонь";
+    char str1[20] = "вода";
+    char str2[20] = "огонь";
     char str3[20] = "";
-    cout << str1 << ", " << str2 <<", "<< str3 << "." << endl;
+    cout << str1 << ", " << str2 <<", "<< str3 << ".\n";
     // функции char
     strcpy_s(str3, str1);
-    cout << str1 << ", " << str2 << ", " << str3 << ".";
+    cout << str1 << ", " << str2 << ", " << str3 << ". \n ";
+    strcat_s(str1, str2);
+    cout << str1 << ", " << str2 << ", " << str3 << ".\n";
+
 
 }
 
